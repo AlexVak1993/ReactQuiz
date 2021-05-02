b47f24e3bceaf723bef51d115fca7e0bcf541048
Alexey Vakulenko <alexvak1993@gmail.com>
2021-05-02 22:00:15 +0300

    components lesson finish

---
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -0,0 +1,24 @@
+import classes from './AnswerItem.css'
+import React from 'react'
+
+
+const AnswerItem = props => {
+  const cls = [classes.AnswerItem]
+
+  if (props.state) {
+    cls.push(classes[props.state])
+  }
+
+  return (
+    <li 
+      className={cls.join(' ')}
+      onClick={() => props.onAnswerClick(props.answer.id)}
+    
+    >
+      { props.answer.text }
+    </li>
+    
+  )
+}
+
+export default AnswerItem
---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,7 @@
+import classes from "./Backdrop.css";
+
+const Backdrop = (props) => (
+  <div className={classes.Backdrop} onClick={props.onClick}></div>
+);
+
+export default Backdrop
